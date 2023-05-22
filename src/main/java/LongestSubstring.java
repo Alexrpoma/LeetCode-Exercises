d399b0be5e854f895d0897394ee2edf2779ca319
Alex Poma <alex.rpoma@outlook.com>
2023-05-22 19:19:58 -0400

    faster solution.

--- src/main/java/LongestSubstring.java
+++ src/main/java/LongestSubstring.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LongestSubstring {
   //Given a string s, find the length of the longest substring without repeating characters.
@@ -10,11 +8,13 @@ public class LongestSubstring {
   // the answer is "abc", with the length 3
 
   public static void main(String[] args) {
-    String s = "abcabcbb";
-    solution(s);
+    String s = "pwwkew";
+    //solution(s);
+    //solution(s);
+    fastSolution(s);
   }
 
-  public static void solution(String s) {
+  public static void partialSolution(String s) {
     String[] letters = s.split("");
     System.out.println(Arrays.toString(letters));
     List<String> list = new ArrayList<>();
@@ -33,5 +33,35 @@ public class LongestSubstring {
     }
     System.out.println(list);
     System.out.println("length: " + list.size());
+  }
+
+  public static void solution(String s) {
+    System.out.println(s);
+    int maxLength = 0;
+    for (int i = 0; i < s.length(); i++) {
+      StringBuilder currentSubString = new StringBuilder();
+      for (int j = i; j < s.length(); j++) {
+        if (currentSubString.indexOf(String.valueOf(s.charAt(j))) != -1) {
+          break;
+        }
+        currentSubString.append(s.charAt(j));
+        maxLength = Math.max(maxLength, currentSubString.length());
+      }
+      System.out.printf("Array: %s, max length: %d\n", currentSubString, maxLength);
+    }
+  }
+
+  public static void fastSolution(String s) {
+    int maxLength = 0;
+    Map<Character, Integer> visitedCharacters = new HashMap<>();
+    for (int right = 0, left = 0; right < s.length(); right++) {
+      char currentCharacter = s.charAt(right);
+      if (visitedCharacters.containsKey(currentCharacter) && visitedCharacters.get(currentCharacter) >= left) {
+        left = visitedCharacters.get(currentCharacter) + 1;
+      }
+      maxLength = Math.max(maxLength, right - left + 1);
+      visitedCharacters.put(currentCharacter, right);
+    }
+    System.out.println(maxLength);
   }
 }
